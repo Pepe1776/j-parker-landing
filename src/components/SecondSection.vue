8201b2fc3f95850c50ddd4787b6c9cf5bc3108e2
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-12-04 12:02:14 -0700

    component

---
+++ src/components/SecondSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="img2-cont">
+<img src="../assets/SAtoshi_art.png" alt="satoshi" class="img2">
+    </div>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+.img2-cont {
+width: 100vw;
+height: 100vh;
+inset: 0;
+}
+
+.img2 {
+  width: 40%;
+  height: auto;
+  object-fit: contain;
+  position: absolute;
+  left: 0%;
+  bottom: 5%;
+  opacity: 0.2;
+  border-radius: 40%;
+  box-shadow: 0px 0px 15px rgba(59, 58, 58, 0.354);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import SecondSection from '../components/SecondSection.vue';
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <SecondSection />
   </main>
 </template>
