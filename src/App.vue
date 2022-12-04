32f31a53edc958ea85751e825b40d71051ed817b
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-12-04 11:54:52 -0700

    home

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,11 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
+<style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  background-color: rbga(59,59,59);
 }
 </style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="img-cont">
-<img src="../assets/SAtoshi_art.png" alt="satoshi shiba inu" class="img satoshi"/>
+    <transition name="slide-fade">
+<img v-show="cue3" src="../assets/jparker_self.png" alt="satoshi shiba inu" class="img satoshi"/>
+</transition>
   </div>
+  <header>
+    <transition name-="slide-fade">
+    <img v-show="cue1" src="../assets/Jparker.png" alt="jparker" class="name">
+  </transition>
+  <transition name="slide-fade">
+    <h2 v-show="cue2" class="sub">Software Engineer, Automation Programmer, UI Designer</h2>
+  </transition>
+  <transition name="slide-fade">
+  <button v-show="cue4" class="btn">Browse Work</button>
+</transition>
+  </header>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const cue1 = ref(false);
+const cue2 = ref(false);
+const cue3 = ref(false);
+const cue4 = ref(false);
+
+setTimeout(() => {
+  cue1.value = true;
+}, 500);
+setTimeout(() => {
+  cue2.value = true;
+}, 800);
+setTimeout(() => {
+  cue3.value = true;
+}, 2000);
+setTimeout(() => {
+  cue4.value = true
+}, 3000);
 </script>
 
 <style scoped>
@@ -16,14 +47,55 @@ inset: 0;
 }
 
 .img {
-  width: 30%;
+  width: 40%;
   height: auto;
   object-fit: contain;
-  position: fixed;
-  left: 0;
-  bottom: 3%;
+  position: absolute;
+  left: 35%;
+  bottom: 15%;
   opacity: 0.2;
+  border-radius: 40%;
+  box-shadow: 0px 0px 15px rgba(59, 58, 58, 0.354);
 }
 
-
+.name {
+  position: absolute;
+  z-index: 2;
+  margin-top: -40%;
+  opacity: 0.3;
+}
+.sub {
+  position: absolute;
+  z-index: 2;
+  margin-top: -28%;
+  margin-left: -4%;
+  opacity: 0.3;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+.btn {
+  position: absolute;
+  z-index: 2;
+  margin-top: -22%;
+  margin-left: 6.5%;
+  opacity: 0.3;
+  box-shadow: 0px 0px 15px rgba(59, 58, 58, 0.354);
+  color: aliceblue;
+  background-color: rgba(59, 58, 58, 0.354);
+  padding: 20px;
+  font-size: x-large;
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  border-radius: 10%;
+}
+.slide-fade-enter-active {
+  transition: all 0.6s ease-out;
+}
+.slide-fade-leave-active {
+  transition: all 1s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(80px);
+  transform: translateY(40px);
+  opacity: 0;
+}
 </style>
